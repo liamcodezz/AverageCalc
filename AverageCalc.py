4ed5e6626ad08156e996483b779876f93895599f
liamcodezz <liamn109801@gmail.com>
2025-03-25 10:09:25 -0700

    Add files via upload

---
+++ AverageCalc.py
@@ -0,0 +1,14 @@
+# define variables
+
+first_digit = int(input("First Number: "))
+second_digit = int(input("Second Number: "))
+
+# find the average score
+
+average = (first_digit / 2) + (second_digit / 2)
+
+# print the variables 
+
+print("Average: " + str(average))
+print(type(average))
+print(int(average))
